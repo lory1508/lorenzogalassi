b2149ae6e5aaf2dd86166e9164502ac5db3df73d
lgalassi <lorenzo.galassi@across.it>
2024-08-12 19:13:59 +0200

    about page

--- pages/lorenzo.vue
+++ pages/lorenzo.vue
@@ -2,11 +2,18 @@
 	<div>
 		<UCard>
 			<template #header>
-				<h1>Qualcosa su di me</h1>
+				<h1 class="text-2xl">Qualcosa su di me</h1>
 
 			</template>
 
-			<Placeholder class="h-32" />
+			<div class="flex flex-row gap-2">
+				<NuxtImg :src="avatar" class="w-48 h-48 rounded-lg shadow-lg shadow-slate-800" />
+				<div class="flex flex-col gap-2">
+					<span>
+						<span class="text-2xl ">C</span>iao, sono <i class="text-primary-400"><strong>Lorenzo</strong></i>, uno sviluppatore web appassionato e creativo con una missione chiara: trasformare idee in esperienze digitali straordinarie. La mia storia è intessuta di codice, design e innovazione, e qui condivido con te il mio viaggio nel mondo della creazione web.
+					</span>
+				</div>
+			</div>
 
 			<template #footer>
 				<Placeholder class="h-8" />
@@ -16,5 +23,5 @@
 </template>
 
 <script setup>
-
+const avatar = '/images/lorenzo.jpg'
 </script>
